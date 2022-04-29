ee84488aa461fe7d1ee2a00a5eb1c23f4404fefc
VICHET.MORM <VICHET.MORM@PNC.LAN>
2022-04-29 15:34:41 +0700

    #5 delete a question

--- public/front/edite_quiz.js
+++ public/front/edite_quiz.js
@@ -1,7 +1,6 @@
 
 
 let dom_questions_view = document.getElementById("questions-view");
-let title =document.getElementById('title');
 let question=document.getElementById('question');
 let answer1=document.getElementById('answer1');
 let answer2=document.getElementById('answer2');
@@ -19,29 +18,32 @@ function add_question(event){
             correct_answer="answer"+(i+1)
         }
     }
-    // console.log(title.value,question.value,answer1.value,answer2.value,answer3.value,answer4.value,correct_answer);
-    let url="/add_question";
-    let body={
-                'title':title.value,
-                'question':question.value,
-                'answers':{
-                    'answer1':answer1.value,
-                    'answer2':answer2.value,
-                    'answer3':answer3.value,
-                    'answer4':answer4.value},
-                    'correctAnswer':correct_answer
-            }
-    axios.post(url,body).then((res)=>{
-        console.log(res);
-    })
-    // clear input
-    clearInput();
-    // display question
-    display_question();
+    if( question.value!=='' && answer1.value!=='' && answer2.value!=='' && answer3.value!=='' && answer3.value!=='' && correct_answer!==''){
+        let url="/add_question";
+        let body={
+                    'question':question.value,
+                    'answers':{
+                        'answer1':answer1.value,
+                        'answer2':answer2.value,
+                        'answer3':answer3.value,
+                        'answer4':answer4.value
+                    },
+                        'correctAnswer':correct_answer
+                }
+        axios.post(url,body).then((res)=>{
+            console.log(res);
+        })
+        // clear input
+        clearInput();
+        // display question
+        display_question();
+    }else{
+        alert("you didn't complete all field!")
+    }
 }
+
 // clear input
 function clearInput(){
-    document.getElementById('title').value="";
     document.getElementById('question').value="";
     document.getElementById('answer1').value="";
     document.getElementById('answer2').value="";
@@ -53,15 +55,19 @@ function clearInput(){
    }
   }
 
-// display the question
+// display the questions
 function display_question(){
     axios.get('/get_question').then((result) => {
         let question_data=result.data;
         console.log(question_data);
-        // Remove the question container 
-        dom_questions_container = document.getElementById("questions-container");
-        dom_questions_container.remove();
         
+        // Remove the question view 
+        while (dom_questions_view.firstChild) {
+            dom_questions_view.removeChild(dom_questions_view.lastChild);
+        }
+
+
+
         for(let i=0;i<question_data.length;i++){
             // create a new card container 
             dom_questions_container = document.createElement("div");
@@ -78,12 +84,18 @@ function display_question(){
             question.textContent=question_data[i].question;
             card_question.appendChild(question);
             // create div for answer1,answer2,answer3,answer4 with class name 'form-check mb-2 border-bottom border-primary border-2'
-            // ans1           
+            // ans1      
+            
             let answer1=document.createElement('div');
             answer1.className="form-check mb-2 border-bottom border-primary border-2";
             card_question.appendChild(answer1);
             let p1=document.createElement('p');
             p1.textContent=question_data[i].answers.answer1;
+            if(question_data[i].correctAnswer=="answer1"){
+                p1.className="text-primary fs-4"
+            }else{
+                p1.className="fs-4";
+            }
             answer1.appendChild(p1);
             // ans2
             let answer2=document.createElement('div');
@@ -91,6 +103,11 @@ function display_question(){
             card_question.appendChild(answer2);
             let p2=document.createElement('p');
             p2.textContent=question_data[i].answers.answer2;
+            if(question_data[i].correctAnswer=="answer2"){
+                p2.className="text-primary fs-4"
+            }else{
+                p2.className="fs-4";
+            }
             answer2.appendChild(p2);
             // ans3
             let answer3=document.createElement('div');
@@ -98,6 +115,11 @@ function display_question(){
             card_question.appendChild(answer3);
             let p3=document.createElement('p');
             p3.textContent=question_data[i].answers.answer3;
+            if(question_data[i].correctAnswer=="answer3"){
+                p3.className="text-primary fs-4"
+            }else{
+                p3.className="fs-4";
+            }
             answer3.appendChild(p3);
             // ans4
             let answer4=document.createElement('div');
@@ -105,17 +127,23 @@ function display_question(){
             card_question.appendChild(answer4);
             let p4=document.createElement('p');
             p4.textContent=question_data[i].answers.answer4;
+            if(question_data[i].correctAnswer=="answer4"){
+                p4.className="text-primary fs-4";
+            }else{
+                p4.className="fs-4";
+            }
             answer4.appendChild(p4);
             // create div with class name 'text-end'
             let dom_btn=document.createElement('div');
-            dom_btn.className="text-end";
+            dom_btn.className="text-end ";
+            dom_btn.id=question_data[i]._id;
             card_question.appendChild(dom_btn)
 
             // create delete button
             let btn_delete=document.createElement('button');
             btn_delete.type="submit";
             btn_delete.className="btn btn-danger m-1";
-            btn_delete.id=question_data[i]._id;
+            btn_delete.id="delete";
             btn_delete.textContent="delete"
             dom_btn.appendChild(btn_delete)
             
@@ -123,7 +151,7 @@ function display_question(){
             let btn_edite=document.createElement('button');
             btn_edite.type="submit";
             btn_edite.className="btn btn-primary";
-            btn_edite.id=question_data[i]._id;
+            btn_edite.id="edite";
             btn_edite.textContent="edite"
             dom_btn.appendChild(btn_edite)
         }
@@ -133,7 +161,31 @@ function display_question(){
     });
     
 }
+function delete_edite_question(e){
+    e.preventDefault();
+  if (e.target.id === "delete") {
+    let isExecuted = confirm("Are you sure to delete this task?");
+    if (isExecuted) {
+      // TODO: Request to the server to detele one task
+      const taskID = e.target.parentElement.id;
+      console.log(taskID);
+      axios.delete("/delete_data/"+taskID).then((response)=>{
+        console.log(response);
+      });       
+
+    }
+  } else if (e.target.id === "edite") {
+    // TODO: Request to the server to update one task as completed
+    const taskID = e.target.parentElement.id;
+    axios.put("/update_data/"+taskID).then((res)=>{
+      console.log(res);
+    })
+  }
+  //DISPLAY TASKS
+  display_question();
+}
 display_question();
 // create btn
 const btn_create_question=document.getElementById('create');
 btn_create_question.addEventListener('click',add_question);
+dom_questions_view.addEventListener("click",delete_edite_question);
